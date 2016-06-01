5b773331179b49961bd38ae2981da06a24049183
Nikita Karetnikov <nkaretnikov@ptsecurity.com>
2016-06-01 13:06:56 +0300

    Add 'default.nix'

---
+++ default.nix
@@ -0,0 +1,19 @@
+{ mkDerivation, array, base, base-orphans, cmdargs, directory
+, filepath, ghc-prim, hspec, lens, mtl, process, profunctors
+, QuickCheck, stdenv, syb, tagged, template-haskell
+}:
+mkDerivation {
+  pname = "HList";
+  version = "0.4.2.0";
+  sha256 = "15bpglqj33n4y68mg8l2g0rllrcisg2f94wsl3n7rpy43md596fd";
+  libraryHaskellDepends = [
+    array base base-orphans ghc-prim mtl profunctors tagged
+    template-haskell
+  ];
+  testHaskellDepends = [
+    array base cmdargs directory filepath hspec lens mtl process
+    QuickCheck syb template-haskell
+  ];
+  description = "Heterogeneous lists";
+  license = stdenv.lib.licenses.mit;
+}
